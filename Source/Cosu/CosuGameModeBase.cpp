d88521d9dbaa6dfe4c0247fcbc17010c71d66f13
Евгений Педан <zqpedan@gmail.com>
2017-03-16 21:47:38 +0300

    Initial commit

---
+++ Source/Cosu/CosuGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Cosu.h"
+#include "CosuGameModeBase.h"
+
+
+
+
---
+++ Source/Cosu/CosuGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "CosuGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COSU_API ACosuGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
